b8af74222ccba84851c8ccf8bac6a3aa56937298
Héricles <hericles.me@icloud.com>
2024-05-06 17:16:52 -0300

    deps: update

--- sections/NewSection.tsx
+++ sections/NewSection.tsx
@@ -1,10 +1,10 @@
 interface Props {
   /**
-  * @description The description of name.
-  */
+   * @description The description of name.
+   */
   name?: string;
 }
 
 export default function TestingPersistency({ name = "World" }: Props) {
-  return <div>Hello {name}</div>
+  return <div>Hello {name}</div>;
 }
